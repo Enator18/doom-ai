e147cbb4c4f895291796a1734ff655680dcc766b
Enator18 <62544405+Enator18@users.noreply.github.com>
2026-03-28 16:50:51 -0400

    added GetPlayerSector and GetSectorNeighbors

--- src/ai_backend.cpp
+++ src/ai_backend.cpp
@@ -1,8 +1,12 @@
 #include <numbers>
 #include <cmath>
+#include <vector>
 
 #include "ai_backend.h"
 #include "p_mobj.h"
+#include "r_defs.h"
+#include "p_maputl.h"
+#include "r_state.h"
 
 // Move the player relative to their facing direction
 // forward and right should be a normalized direction
@@ -22,6 +26,7 @@ void MovePlayerWorld(player_t* player, float x, float y)
     MovePlayerLocal(player, forward, right);
 }
 
+// Move the player towards a position
 void MovePlayerTowards(player_t* player, float x, float y)
 {
     float playerX = FixedToFloat(player->mo->x);
@@ -35,6 +40,51 @@ void MovePlayerTowards(player_t* player, float x, float y)
         distY /= distance;
     }
     MovePlayerWorld(player, distX, distY);
+}
+
+// Get the sector that the player is currently in
+sector_t* GetPlayerSector(player_t* player)
+{
+    return player->mo->subsector->sector;
+}
+
+struct SectorEdge
+{
+    sector_t* otherSector;
+    float x;
+    float y;
+};
+
+std::vector<SectorEdge> GetSectorNeighbors(sector_t* sector)
+{
+    std::vector<SectorEdge> neighbors;
+    for (uint32_t i = 0; i < sector->linecount; i++)
+    {
+        line_t *line = sector->lines[i];
+
+        P_LineOpening(line);
+        if (openrange < IntToFixed(56))
+        {
+            continue;
+        }
+        if (openbottom - sector->floorheight > IntToFixed(24))
+        {
+            continue;
+        }
+        sector_t* other = sides[line->sidenum[sides[line->sidenum[0]].sector == sector]].sector;
+        if (other->special == 4 || other->special == 5 || other->special == 7 || other->special == 11)
+        {
+            continue;
+        }
+        SectorEdge edge
+        {
+            .otherSector = other,
+            .x = (FixedToFloat(line->v1->r_x) + FixedToFloat(line->v2->r_x)) / 2,
+            .y = (FixedToFloat(line->v1->r_y) + FixedToFloat(line->v2->r_y)) / 2
+        };
+        neighbors.push_back(edge);
+    }
+    return neighbors;
 }
 
 // The main entry point for all AI logic. Call all AI systems from here.
